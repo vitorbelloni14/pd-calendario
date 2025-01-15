998163259bfabd3e4e5e0f0782a9d75d58350c51
Vitor Ehrich Belloni <vbelloni@ip-172-19-7-196.us-east-2.compute.internal>
2025-01-15 11:53:00 -0300

    feat: configurações iniciais e componente de calendário

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib';
+import * as components from 'vuetify/lib/components';
+import * as directives from 'vuetify/lib/directives';
+
+Vue.use(Vuetify, {
+  components,
+  directives
+});
+
+export default new Vuetify({});
+
